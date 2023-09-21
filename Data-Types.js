58495625c47fdab8c9a8519a40b8dcbceba623da
Muhammad Imran <imran5244513@gmail.com>
2023-09-21 04:44:59 -0700

    add files via upload
    First Training

---
+++ Data-Types.js
@@ -0,0 +1,153 @@
+// js: data types, type system
+
+// Primitives: 
+// number, string, boolean, undefined
+
+// collection: 
+// array, object
+
+
+
+
+
+// // dynmaically typed langage
+// let x = 10;
+// console.log(x);
+// console.log(typeof x);
+// x = "Lahore";
+// console.log(x);
+// console.log(typeof x);
+// x = true;
+// console.log(x);
+// console.log(typeof x);
+
+
+
+
+// let y = true;
+// console.log(y);
+// console.log(typeof y);
+
+
+// let z = 10.98;
+// console.log(z);
+// console.log(typeof z);
+
+// let title = "Mobile Phone";
+// console.log(title);
+// console.log(typeof title);
+
+
+// // uninit is undefined
+// let a;
+// console.log(a);
+// console.log(typeof a);
+
+// let active = false;
+// console.log(active);
+// console.log(typeof active);
+
+
+
+// // Array - index/sub
+
+// let array = [10, "lahore", true, 45, "asdfasdf"];
+// console.log(array);
+
+
+// let marks = [10, 30, 50, 12, 56];
+// marks[10] = 40;
+// console.log(marks)
+// console.log(marks[7])
+// console.log(marks.length);
+
+// // dynamic
+// let marks = [];
+// marks[0] = 90;
+// marks[1] = 80;
+// marks[2] = 40;
+// marks[3] = 10;
+// console.log(marks);
+
+// // undefined
+// let marks = [10, 30, 50, 12, 56];
+// console.log(marks[50]);
+
+// let marks = [10, 30, 50, 12, 56];
+// console.log(marks[4]);
+
+
+
+
+
+
+// // object - collection of properties (key:value pairs)
+// // property/field: key:value pairs
+// let product = {
+//     id: 1,
+//     title: "Mobile Phone",
+//     price: 50000,
+//     avail: true
+// };
+
+// console.log(product);
+// console.log(product.price);
+
+
+
+// // Real example
+
+// let produc =
+// {
+//     id: 1,
+//     title: "Mobile Phone",
+//     price: 50000,
+//     avail: true,
+//     // units: 50,
+//     colors : [
+//         {id: 1, color: "Black", units: 30},
+//         {id: 2, color: "Red", units: 20},
+//     ]
+// }
+
+// console.log(produc.colors[0].units);
+// // console.log(product);
+
+
+// //When many variance
+
+// let products = [
+//     {
+//         id: 1,
+//         title: "Mobile Phone",
+//         price: 50000,
+//         avail: true,
+//         // units: 50,
+//         colors : [
+//             {id: 1, color: "Black", units: 30},
+//             {id: 2, color: "Red", units: 20},
+//         ]
+//     },
+//     {
+//         id: 2,
+//         title: "Mobile Phone",
+//         price: 50000,
+//         avail: true,
+//         // units: 50,
+//         colors : [
+//             {id: 1, color: "Black", units: 30},
+//             {id: 2, color: "Red", units: 20},
+//         ]
+//     },
+//     {
+//         id: 3,
+//         title: "Mobile Phone",
+//         price: 50000,
+//         avail: true,
+//         // units: 50,
+//         colors : [
+//             {id: 1, color: "Black", units: 30},
+//             {id: 2, color: "Red", units: 20},
+//         ]
+//     }
+// ]
